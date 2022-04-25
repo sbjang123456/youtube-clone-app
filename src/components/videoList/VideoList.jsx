d7e796f1cba64a544826f1f56d2f4573fb47f585
sbjang <sbjang123456@gmail.com>
2022-04-26 01:14:18 +0900

    add styling (css) with item, list

--- src/components/videoList/VideoList.jsx
+++ src/components/videoList/VideoList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import VideoItem from '../videoItem/VideoItem';
+import styles from './VideoList.module.css';
 
 const VideoList = (props) => {
   
   return (
-    <ul>
+    <ul className={styles.videos}>
       {props.videos.map(video => <VideoItem key={video.id} video={video} />)}
     </ul>
   );
